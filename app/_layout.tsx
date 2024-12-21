6cf2844b6cc8288d5c63a92c5afd09a7fa98cf2f
Jonas Sourlier <jonas.sourlier@yolp.ch>
2024-12-21 13:10:27 +0100

    simulate longer loading time

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -19,7 +19,9 @@ export default function RootLayout() {
 
   useEffect(() => {
     if (loaded) {
-      SplashScreen.hideAsync();
+      setTimeout(() => {
+        SplashScreen.hideAsync();
+      }, 1000);
     }
   }, [loaded]);
 
